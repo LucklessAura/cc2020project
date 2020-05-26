f2bcb0a7b98ece00b36c82c3d37bd37fad809272
cristian98st <44675214+cristian98st@users.noreply.github.com>
2020-05-26 23:03:36 +0300

    Update.

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 var express = require('express');
 const mysql = require('promise-mysql');
 
+
 var app = express();
+
+const io = require('socket.io')(3000)
+
 const cors = require('cors');
+
 app.use(cors());
-app.use(express.static('public/pages'));
+app.use(express.static('./public/pages'));
+app.use(express.urlencoded({ extended: true }))
+app.set('views', './public/pages')
+app.set('view engine', 'ejs')
 
 pool = "";
+const rooms = {}
+const clientsAndSockets = []
 
 async function initDatabase() {
     const createPool = async() => {
@@ -36,6 +46,63 @@ async function initDatabase() {
 
 initDatabase();
 
+async function checkAppointments() {
+    var isFixedTime = false;
+    var date = new Date();
+    while (isFixedTime == false){
+        if(date.getMinutes() == 0)
+            isFixedTime = true;
+    }
+    while (true) {
+        var date = Date.now()
+        pool.query("call get_appointments()", function(err, results) {
+            if (err) {
+                console.log("Error retrieving appointments.");
+            }
+            else {
+                results.forEach(result => {
+                    db_date = new Date(result.date)
+                    db_date.setMinutes(0);
+                    date.setMinutes(0);
+                    if(db_date == date){
+                        createNewRoom(result.doctor, result.patient, result.date);
+                    }
+                });
+            }
+        })
+        await new Promise(resolve => setTimeout(resolve, 3600000));
+    }
+}
+
+checkAppointments();
+
+async function createNewRoom(doctor, patient, time){
+    var room_name = hashCode("doctor" + "patient");
+
+    rooms[room_name] = { users: {} }
+    var sent = 0;
+    clientAndSockets.forEach(cs => {
+        if(cs.name == doctor || cs.name == patient) {
+            io.sockets.socket(cs.socket).emit("room-invite", room_name);
+            sent += 1;
+        }
+        if (sent == 2)
+            break;
+    });    
+} 
+
+async function hashCode(str) {
+    return str.split('').reduce((prevHash, currVal) =>
+      (((prevHash << 5) - prevHash) + currVal.charCodeAt(0))|0, 0);
+  }
+
+app.get("/test", function(req, res) {
+    res.render("createRoom", {rooms: rooms})
+});
+
+app.get('/', (req, res) => {
+    res.render('createRoom', {rooms: rooms})
+})
 
 app.get("/getHospitals.html", async function(req, res) {
     var county_code = req.headers["county_code"];
@@ -54,6 +121,90 @@ app.get("/getHospitals.html", async function(req, res) {
     });
 
 })
+
+app.get("/getAllHospitals", async function(req, res) {
+    pool.query("CALL get_hospitals_list()", (err, results, fields) => {
+        if(err){
+            console.log("Error in retrieving hospitals list.");
+        }
+
+        res.send(results);
+    })
+})
+
+app.get("/getDoctors/:hospital", (req, res) => {
+    pool.query("CALL get_hospital_doctors(?)", req.params.hospital, (err, res, fields) => {
+        if(err){
+            console.log("Error in retrieving doctors list.");
+        }
+
+        res.send(res);
+    })
+})
+
+
+app.post('/room', (req, res) => {
+    if (rooms[req.body.room] != null){
+        return res.redirect('/')
+    }
+    rooms[req.body.room] = { users: {} }
+    res.redirect(req.body.room)
+    io.emit('room-created', req.body.room)
+})
+
+app.get('/:room', (req, res) => {
+    if (rooms[req.params.room] == null){
+        return res.redirect('/')
+    }
+    res.render('room', { roomName: req.params.room})
+})
+
+app.get('/getTime/:doctor', (req, res) => {
+    for (var i = 0; i < clientsAndSockets.length; i++){
+        if(clientsAndSockets[i].name = req.params.doctorName){
+            io.sockets.socket(clientsAndSockets[i].socket).emit('request-doctor-availability');
+            io.sockets.socket.on('response-doctor-availability', dates => {
+                res.send(dates)
+            });
+        }
+    }
+})
+
+app.post('/finishAppointment', (req, res) => {
+    for (var i = 0; i < clientsAndSockets.length; i++){
+        if(clientsAndSockets[i].name = req.params.doctor){
+            io.to(clientsAndSockets.socket).emit('doctor-add-appointment', {start: req.params.start, end: req.params.end} );
+        }
+    }
+
+    pool.query("CALL insert_appointment(?,?,?)", [req.params.start, req.params.doctor, req.params.patient])
+})
+
+io.on('connection', socket => {
+    socket.on('new-user', (room, name) => {
+        socket.join(room)
+        rooms[room].users[socket.id] = name
+        socket.to(room).broadcast.emit('user-connected', name)
+    })
+    socket.on('send-chat-message', (room, message) => {
+        socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id]})
+    })
+    socket.on('disconnect', () => {
+        getUserRooms(socket).forEach(room => { 
+            socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
+            delete rooms[room].users[socket.id]
+        });
+    })
+});
+
+function getUserRooms(socket) {
+    return Object.entries(rooms).reduce((names, [name, room]) => {
+        if(room.users[socket.id] != null){
+            names.push(name)
+        } 
+        return names
+    }, [])
+}
 
 
 module.exports = app;
---
+++ public/pages/javascripts/appointment.js
@@ -0,0 +1,253 @@
+const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
+const roomContainer = document.getElementById('room-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+
+if (messageForm != null){
+    const name = prompt("What is your name?")
+    
+    appendMessage('You joined')
+    socket.emit('new-user', roomName, name)
+
+    messageForm.addEventListener('submit', e => {
+        e.preventDefault()
+        const message = messageInput.value
+        appendMessage(`You: ${message}`)
+        socket.emit('send-chat-message', roomName, message)
+        messageInput.value = ''
+    })
+}
+
+socket.on('chat-message', data => {
+     appendMessage(`${data.name}: ${data.message}`)
+});
+
+socket.on('user-connected', name => {
+    appendMessage(`${name} connected`)
+});
+
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} disconnected`)
+});
+
+socket.on('room-created', room => {
+    console.log("kjsdhf");
+    const roomElement = document.createElement('div')
+    roomElement.innerText = room
+    const roomLink = document.createElement('a')
+    roomLink.href = `/${room}`
+    roomLink.innerText = "Join"
+    roomContainer.append(roomElement)
+    roomContainer.append(roomLink)
+});
+
+socket.on('request-doctor-availability', () => {
+    var dates = getDoctorAvailability();
+    socket.emit("response-doctor-availability", dates);
+})
+
+socket.on('doctor-add-appointment', data =>{
+    insertAppointment(data.start, data.end, true);
+})
+
+socket.on('room-invite', roomName => {
+    if(confirm("You are invited to the appointment.")) {
+        window.location.href = '/' + roomName
+    }
+})
+
+function appendMessage(message){
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+}
+
+      
+function authenticate() {
+    return gapi.auth2.getAuthInstance()
+        .signIn({scope: "https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.events"})
+        .then(function() { console.log("Sign-in successful"); },
+              function(err) { console.error("Error signing in", err); });
+} 
+
+function loadClient() {
+    gapi.client.setApiKey('AIzaSyCLMI60gh3A2gk8NGXtsB8_HUymue0ERpU');
+    return gapi.client.load("https://content.googleapis.com/discovery/v1/apis/calendar/v3/rest")
+        .then(function() { 
+            console.log("GAPI client loaded for API");
+            var authButton = document.getElementById("authBtn").setAttribute("hidden", "hidden");
+            var submitBtn = document.getElementById("submitBtn").removeAttribute("hidden"); 
+        },  
+        function(err) { console.error("Error loading GAPI client for API", err); });
+}
+
+function checkAvailability() {
+    var returnedMessage = document.getElementById("returnedMessage");
+    var dateSelect = document.getElementById("dates");
+    var eventDate = new Date(dateSelect.options[dateSelect.selectedIndex].text);  
+    var eventStartTime = new Date(eventDate);
+    var eventEndTime = new Date (eventDate);
+    eventEndTime.setMinutes(eventEndTime.getMinutes() + 60);
+    console.log(eventStartTime);
+    console.log(eventEndTime);
+    
+    return gapi.client.calendar.freebusy.query({
+        "resource":{
+            timeMin: eventStartTime,
+            timeMax: eventEndTime,
+            timeZone: 'Europe/Bucharest',
+            items: [{id: 'primary'}]
+        }
+    })
+        .then(
+        function(response){
+            console.log(response)
+            var eventsArr = response.result.calendars.primary.busy;
+        
+            if(eventsArr.length === 0) 
+                insertAppointment(eventStartTime, eventEndTime, false);
+            else{
+                returnedMessage.innerHTML = "You are not free during " + eventStartTime + " and " + eventEndTime
+            }
+        },
+        function(err){
+            returnedMessage.innerHTML = "Time validation error.";
+            console.error("Time validation error", err);
+    });
+}
+
+function getDoctorAvailability() {
+    var dates = []
+    
+    var startTime = new Date();
+    startTime.setHours(startTime.getHours() + 1);
+    startTime.setMinutes(0);
+    var endTime = startTime;
+    endTime.setHours(endTime.getHours() + 1);
+    
+    while (dates.length < 10)
+    {
+        gapi.client.calendar.freebusy.query({
+            "resource":{
+                timeMin: startTime,
+                timeMax: endTime,
+                timeZone: 'Europe/Bucharest',
+                items: [{id: 'primary'}]
+            }
+        })
+        .then(
+            function(response){
+                var eventsArr = response.result.calendars.primary.busy;
+            
+                if(eventsArr.length === 0) 
+                    date.add(startTime);
+            },
+            function(err){}
+        );
+
+        startTime.setHours(startTime.getHours() + 1);
+        endTime.setHours(endTime.getHours() + 1);
+    }
+
+    return dates;
+}
+
+function insertAppointment(eventStartTime, eventEndTime, isDoctor) {
+    var returnedMessage = document.getElementById("returnedMessage");
+    var doctors = document.getElementById("doctors");
+    var doctor = doctors.options[doctors.selectedIndex].value;
+      
+    const event = {
+        summary: 'Medical Appointment',
+        location: 'Medical Center Location',
+        description: 
+        'Your medical appointment is set on ' + eventStartTime,
+        start: {
+            dateTime: eventStartTime,
+            timeZone: 'Europe/Bucharest'
+        },
+        end: {
+            dateTime: eventEndTime,
+            timeZone: 'Europe/Bucharest'
+        },
+        colorId: 10,
+    } 
+
+    return gapi.client.calendar.events.insert({calendarId: 'primary', resource: event})
+        .then(function(response) {
+            if(!isDoctor) {
+                returnedMessage.innerHTML = ("Event added.");
+                $.post("./finishAppointment", {
+                    doctor: doctor,
+                    start: eventStartTime,
+                    end: eventEndTime
+                }, function(data, status){
+                })
+            }
+        },
+        function(err) {
+            if(!isDoctor)
+                returnedMessage.innerHTML = "Execute error";
+        });
+}
+
+gapi.load("client:auth2", function() {
+    gapi.auth2.init({client_id: "813562380833-v0273o7adbgtedm4s3udrurdiphjpfm6.apps.googleusercontent.com"});
+});
+
+function populateHospitals() {
+    var selectHospitals = document.getElementById("hospitals")
+    $.get("/getAllHospitals", hospitals => {
+        hospitals.forEach(hospital => {
+            var createdOption = document.createElement("option");
+            createdOption.text = hospital;
+            selectHospitals.add(createdOption);
+        });
+    })
+}
+
+function populateDoctors() {
+    var retrievedDoctors = [];
+    var selectHospitals = document.getElementById("hospitals")
+    var hospital = selectHospitals.options[selectHospitals.selectedIndex].value;
+    var labelDoctors = document.getElementById("doctors-label")
+    var selectDoctors = document.getElementById("doctors")
+
+    var url = "/getDoctors/" + hospital;
+    $.get(url, doctors => {
+        retrievedDoctors.forEach(doctor => {
+            var createdOption = document.createElement("option");
+            createdOption.text = doctor;
+            selectDoctors.add(createdOption);
+        });
+    })
+
+    labelDoctors.hidden = false
+    selectDoctors.hidden = false;
+}
+
+function populateTime() {
+    var retrievedAvailableDates = [];
+    var selectDoctors = document.getElementById('doctors')
+    var doctor = selectDoctors.options[selectDoctors.selectedIndex].value;
+    var labelDate = document.getElementById("dates-label")
+    var selectDate = document.getElementById("dates")
+    var url = "/getTime/" + doctor;
+    $.get(url, function (data, status) {
+        retrievedAvailableDates = data;
+        retrievedAvailableDates.forEach(date => {
+            var createdOption = document.createElement("option");
+            createdOption.text = date;
+            selectDate.add(createdOption);
+        });
+    })
+
+    labelDate.hidden = false;
+    selectDate.hidden = false;
+}
+
+function makeSubmitAvaialable() {
+    document.getElementById("submit-button").disabled = false;
+}
