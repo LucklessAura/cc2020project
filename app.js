8fa6ef282dc1548909215c738766b1540769017d
AndreiG <48528839+AndreiGhiran@users.noreply.github.com>
2020-05-27 05:23:42 +0300

    Pagina "Cont" acum afiseaza datele userului si ofera posibilitatea de a le edita

--- app.js
+++ app.js
@@ -55,6 +55,35 @@ app.get("/getHospitals.html", async function(req, res) {
 
 })
 
+
+app.get("/getUser.html", async function(req, res) {
+    var first_name = req.headers["first_name"];
+    var last_name = req.headers["last_name"];
+    var rol = req.headers["rol"];
+    
+    console.log("<-!received a /getUser request for " + rol + " " + last_name + " " + first_name + "!-->");
+
+    if(rol=="doctor"){
+        pool.query("CALL get_doctor(?,?)", [last_name, first_name], (err, result, fields) => {
+            if (err) {
+                return console.error(err.message);
+            }
+            console.log(result[0][0]);
+            res.send(result[0][0]);
+            res.end();
+        });
+    }else{
+        pool.query("CALL get_patient(?,?)", [last_name, first_name], (err, result, fields) => {
+            if (err) {
+                return console.error(err.message);
+            }
+            console.log(result[0][0]);
+            res.send(result[0][0]);
+            res.end();
+        });
+    }
+})
+
 app.get("/existsUser.html",async function(req, res){
     var first_name = req.headers["first_name"];
     var last_name = req.headers["last_name"];
@@ -129,4 +158,36 @@ app.get("/createUser.html", async function(req, res) {
 
     
 })
+
+app.get("/updateInfo.html", async function(req, res) {
+    var first_name = req.headers["first_name"];
+    var last_name = req.headers["last_name"];
+    var rol = req.headers["rol"];
+    var tel = req.headers['tel'];
+    var email = req.headers['email'];
+
+    console.log("<-!received a /updateInfo request for " + rol + " " + last_name + " " + first_name + "!-->");
+
+    if(rol=="doctor"){
+        var special = req.headers['special'];
+        pool.query("UPDATE doctors SET phone_number = ?,mail=?,speciality =? where first_name = ? and last_name=?", [tel,email,special, first_name,last_name], (err, result, fields) => {
+            if (err) {
+                return console.error(err.message);
+            }
+            res.send("\t|-->Update Succesfull");
+            res.end();
+        });
+    }else{
+        pool.query("UPDATE patients SET phone_number = ?,mail=? where first_name = ? and last_name=?", [tel,email, first_name,last_name], (err, result, fields) => {
+            if (err) {
+                return console.error(err.message);
+            }
+            res.send("\t|-->Update Succesfull");
+            res.end();
+        });
+    }
+    location.replace('./account.html');
+})
+
+
 module.exports = app;
